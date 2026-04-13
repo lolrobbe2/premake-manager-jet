e692f267d39c85f8fa6673dd5e276892cc22d5d6
GitHub Action <action@github.com>
2026-04-13 20:59:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/lolrobbe2/premakemanagerjet/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lolrobbe2/premakemanagerjet/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.lolrobbe2.premakemanagerjet.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lolrobbe2.premakemanagerjet.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
